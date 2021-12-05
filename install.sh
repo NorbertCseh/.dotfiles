d03f8630ec5d81dd9b782688e203020e2072a5f0
NorbertCseh <cseh.norbert94@gmail.com>
2021-12-05 11:35:11 +0100

    Fix homebrew install

--- install.sh
+++ install.sh
@@ -1,36 +1,47 @@
-# install brew
+echo "Updae system"
+sudo apt update
+sudo apt upgrade
+
+echo "Install brew"
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-# setup brew
-echo 'eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> ~/.zprofile
-eval "$(home/linuxbrew/.linuxbrew/bin/brew shellenv)"  /
+echo "Setup brew for bash"
+echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> ~/.profile
+eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+sudo apt-get install build-essential
+brew install gcc
 
-# install packages with brew
+echo "Install packages"
 brew install \
-  zsh \
-  antibody \
-  git \
-  neovim \
-  tmux \
-  stow \
-  yarn \
-  fzf \
-  ripgrep \
-  bat \
-  direnv \
-  npm \
-  node
+    zsh \
+    antibody \
+    git \
+    neovim \
+    tmux \
+    stow \
+    yarn \
+    fzf \
+    ripgrep \
+    bat \
+    direnv \
 
-# stow
+echo "Add zsh to login shells"
+command -v zsh | sudo tee -a /etc/shells
+
+echo "Use zsh as default shell"
+sudo chsh -s $(which zsh) $USER
+
+echo "Stow packages"
 stow git
 stow zsh
 stow nvim
 
-# add zsh to valid login shells 
-command -v zsh | sudo tee -a /etc/shells
+echo "Setup brew for zsh"
+echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> ~/.zprofile
+eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+sudo apt-get install build-essential
+brew install gcc
 
-# use zsh as default shell
-sudo chsh -s $(which zsh) $USER
 
-# bundle zsh plugins
-antibody bundle < ~/.zsh_plugins.txt > ~/.zsh_plugins.sh
+echo "Bundle zsh plugins"
+antibody bundle < ~/.zsh_plugins.txt > ~/.zsh_plugins.sht
