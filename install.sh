1ebe0ae8d07c6537b06bada792fda0165d152405
Norbertcseh <cseh.norbert94@gmail.com>
2021-12-05 13:08:54 +0100

    Reverint back to nix

--- install.sh
+++ install.sh
@@ -1,46 +1,40 @@
-echo "Updae system"
+"Update system"
 sudo apt update
 sudo apt upgrade
 
-echo "Install brew"
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+echo "Install nix"
+curl -L https://nixos.org/nix/install | sh
 
-echo "Setup brew for bash"
-echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> ~/.profile
-eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
-sudo apt-get install build-essential
-brew install gcc
+echo "Source nix"
+. ~/.nix-profile/etc/profile.d/nix.sh
 
 echo "Install packages"
-brew install \
-    zsh \
-    antibody \
-    git \
-    neovim \
-    tmux \
-    stow \
-    yarn \
-    fzf \
-    ripgrep \
-    bat \
-    direnv \
+nix-env -iA \
+	nixpkgs.zsh \
+	nixpkgs.antibody \
+	nixpkgs.git \
+	nixpkgs.neovim \
+	nixpkgs.tmux \
+	nixpkgs.stow \
+	nixpkgs.yarn \
+	nixpkgs.fzf \
+	nixpkgs.ripgrep \
+	nixpkgs.bat \
+	nixpkgs.direnv
 
-echo "Add zsh to login shells"
-command -v zsh | sudo tee -a /etc/shells
-
-echo "Use zsh as default shell"
-sudo chsh -s $(which zsh) $USER
-
-echo "Setup brew for zsh"
-echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> ~/.zprofile
-eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
-sudo apt-get install build-essential
-brew install gcc
 
 echo "Stow packages"
 stow git
 stow zsh
 stow nvim
 
+echo "Add ush to login shells"
+command -v zsh | sudo tee -a /etc/shells
+
+echo "Use zsh as default shell"
+sudo chsh -s $(which zsh) $USER
+
 echo "Bundle zsh plugins"
-antibody bundle < ~/.zsh_plugins.txt > ~/.zsh_plugins.sht
+antibody bundle < ~/.zsh_plugins.txt > ~/.zsh_plugins.sh
+
+exit
