3ddc1d87b200676b859bf061a3fb342ae9ca3b75
NorbertCseh <cseh.norbert94@gmail.com>
2021-12-05 11:41:45 +0100

    order fix

--- install.sh
+++ install.sh
@@ -31,17 +31,16 @@ command -v zsh | sudo tee -a /etc/shells
 echo "Use zsh as default shell"
 sudo chsh -s $(which zsh) $USER
 
-echo "Stow packages"
-stow git
-stow zsh
-stow nvim
-
 echo "Setup brew for zsh"
 echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> ~/.zprofile
 eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 sudo apt-get install build-essential
 brew install gcc
 
+echo "Stow packages"
+stow git
+stow zsh
+stow nvim
 
 echo "Bundle zsh plugins"
 antibody bundle < ~/.zsh_plugins.txt > ~/.zsh_plugins.sht
